f1575cd29f6613fedaf424bef9597da3132118bd
WS-2001 <78418418+WS-2001@users.noreply.github.com>
2024-02-18 07:29:29 +0000

    Initial Commit

---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Wares on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
